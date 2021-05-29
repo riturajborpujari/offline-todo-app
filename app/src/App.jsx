dbf50c7f18353b7774d0608b58318a6527e82eb5
Rituraj Borpujari <riturajborpujari@outlook.com>
2021-05-29 19:37:09 +0530

    fix(apollo): provider was already moved to index.js

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,15 +1,10 @@
-import {ApolloProvider} from '@apollo/client';
-
-import client from './graphql/client';
 import AppLayout from './layouts/AppLayout';
 import Homepage from './pages/Homepage';
 
 export default function App() {
   return (
     <AppLayout>
-      <ApolloProvider client={client}>
-        <Homepage />
-      </ApolloProvider>
+      <Homepage />
     </AppLayout>
   );
 }
