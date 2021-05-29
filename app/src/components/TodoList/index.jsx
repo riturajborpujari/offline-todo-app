1b3deec1d5a251ae9ac3cc17bceedd8ff8f44624
Rituraj Borpujari <riturajborpujari@outlook.com>
2021-05-29 19:42:48 +0530

    feat(offline): added optimistic responses for update and delete

--- app/src/components/TodoList/index.jsx
+++ app/src/components/TodoList/index.jsx
@@ -9,8 +9,12 @@ import Section from '../../lib/Section';
 import Todo from '../Todo';
 import styles from './index.module.css';
 import Icon from '../../lib/Icon';
+import { useEffect, useState } from 'react';
 
 export default function TodoList() {
+  const [todo_delete_id, SetTodoForDeletion] = useState(null);
+  const [todo_update, SetTodoForUpdate] = useState({ id: null, checked: false });
+
   const { error, loading, data } = useQuery(LIST_TODOS);
   const [updateTodoCheckState] = useMutation(UPDATE_TODO_CHECK_STATE);
   const [removeTodo] = useMutation(REMOVE_TODO, {
@@ -28,24 +32,60 @@ export default function TodoList() {
           }
         }
       });
-    }
+    },
+
   })
+
+  useEffect(() => {
+    if (todo_delete_id) {
+      removeTodo({
+        variables: { id: todo_delete_id },
+        optimisticResponse: {
+          result: {
+            __typename: 'todo',
+            id: todo_delete_id
+          }
+        }
+      })
+        .then(_ => {
+          SetTodoForDeletion(null);
+        })
+        .catch(err => {
+          console.log('Delete error: ', err);
+        })
+    }
+  }, [todo_delete_id, removeTodo]);
+
+  useEffect(() => {
+    if (todo_update && todo_update.id) {
+      updateTodoCheckState({
+        variables: { id: todo_update.id, checked: !todo_update.checked },
+        optimisticResponse: {
+          todo: {
+            __typename: 'todo',
+            id: todo_update.id,
+            checked: !todo_update.checked
+          }
+        }
+      })
+        .then(_ => {
+          SetTodoForUpdate({ id: null });
+        })
+        .catch(err => {
+          console.log('Update error: ', err);
+        })
+    }
+  }, [todo_update, updateTodoCheckState]);
 
   if (error) return <p className={styles.error}>{error.message}</p>;
   if (loading) return <p className={styles.info}>Loading...</p>;
 
   const handleCheckIconClick = (todo_id, curr_checked) => {
-    updateTodoCheckState({ variables: { id: todo_id, checked: !curr_checked } })
-      .catch(err => {
-        console.log('Update error: ', err);
-      })
+    SetTodoForUpdate({ id: todo_id, checked: curr_checked });
   }
 
   const handleDeleteIconClick = (todo_id) => {
-    removeTodo({ variables: { id: todo_id } })
-      .catch(err => {
-        console.log('Delete error: ', err);
-      })
+    SetTodoForDeletion(todo_id);
   }
 
   return (
